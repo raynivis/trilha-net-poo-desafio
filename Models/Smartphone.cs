5b5e8f8ff100acd630d33671c9ced492f3159f48
raynivis <raysantiagodel20@gmail.com>
2023-12-26 21:42:04 -0300

    Desafio concluidogit add Program.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // DONE: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; }
+        private string IMEI { get;  }
+        private string Memoria { get; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            // DONE: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// DONE: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+
+Iphone i = new Iphone(numero: "18991335474", modelo: "Iphone 8", imei:"5555555555555", memoria:"128gb");
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("Smartphone Nokia:");
+
+Nokia no = new Nokia(numero: "18991369854", modelo: " Nokia Lumia 1320", imei:"777777777777", memoria:"32gb");
+no.Ligar();
+no.ReceberLigacao();
+no.InstalarAplicativo("Reddit");
